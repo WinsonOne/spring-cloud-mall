b7f478f95264768cdad605cb5a38a499dda7644b
wenweicong <wenweicong@96900.com.cn>
2019-07-18 17:16:35 +0800

    eureka注册中心初始化代码提交

---
+++ mall-eureka-server/src/main/java/com/mall/eureka/MallEurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.mall.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author TheOne
+ * 项目启动类
+ */
+@SpringBootApplication
+public class MallEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallEurekaServerApplication.class, args);
+	}
+
+}
