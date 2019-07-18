f25dd38302ca0bf70c4f53956606e644c1ce8ab9
Theone <648181015@qq.com>
2019-07-18 21:52:24 +0800

    eureka单节点启动代码提交

--- mall-eureka-server/src/main/java/com/mall/eureka/MallEurekaServerApplication.java
+++ mall-eureka-server/src/main/java/com/mall/eureka/MallEurekaServerApplication.java
@@ -2,12 +2,14 @@ package com.mall.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author TheOne
  * 项目启动类
  */
 @SpringBootApplication
+@EnableEurekaServer
 public class MallEurekaServerApplication {
 
 	public static void main(String[] args) {
